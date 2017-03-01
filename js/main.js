e3cd7d37755fb673041363915abad9168c95aa1a
Scott O'Neil <ScottO@Scotts-MacBook-Pro.local>
2017-03-01 15:41:19 -0500

    Changes made due to requirements

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 //JavaScript file
+
+var catNameOne = "Catty";
+var catNameTwo = "Spidey";
+
 $(document).ready(function() {
-  $("#catclick").click(function() {
+  $("#catclickone").click(function() {
       $('#output').html(function(i, val) { return +val+1 });
   });
-
+  $("#catclicktwo").click(function() {
+      $('#output').html(function(i, val) { return +val+1 });
+  });
+  $("#catOne").prepend("<h1>" + catNameOne + "</h1>");
+  $("#catTwo").prepend("<h1>" + catNameTwo + "</h1>");
 });
